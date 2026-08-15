53ad64cce219fe664223da5a163fbce1052402ca
whinder251 <castrillowhinder@gmail.com>
2026-08-15 17:13:21 -0400

    Autenticacion con django-allauth y django-axes, modelo de usuario personalizado

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
