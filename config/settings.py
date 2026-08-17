02a70f2ad4855dc8e4b41f2192ca3ad2692f19f4
whinder251 <castrillowhinder@gmail.com>
2026-08-16 21:10:05 -0400

    ver errores

--- config/settings.py
+++ config/settings.py
@@ -177,3 +177,24 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
